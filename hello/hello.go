33e39e9d59566043c418bcf02a31d1b41c5f16d1
Jamie <1866+treejamie@users.noreply.github.com>
2025-02-15 08:11:12 +0000

    2 - Greeting People (#2)
    https://go.dev/doc/tutorial/create-module

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// set the log up
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// make a slice of names - make sure clarence is last
+	// Clarence only makes an appearance in tests.
+	names := []string{"Big lez", "Sassy", "Mike Nolan", "Donny", "Wayne", "Norton", "Quinton"}
+
+	// get the greetings for the slice of names
+	message, err := greetings.Hellos(names)
+
+	// handle the error if there was one
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
