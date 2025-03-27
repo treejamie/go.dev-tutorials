134b5181a044aaacf0c0bf164752b642933d196f
treejamie <me@treejamie.com>
2025-03-27 07:19:17 +0000

    import everything in parenthesis, and names are only exported in a package if they are uppercase. I look forward to spending hours re-learning that whilst debugging

--- tour/packages.go
+++ tour/packages.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 )
 
+// ^ individual imports can be used, but the preferred style is the grouped parenthesis approach
+
 func main(){
 	fmt.Println("My favourite number is", rand.Intn(10))
 }
