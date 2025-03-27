0c49af4b0271febfcbcfb01f5302e83391cb41af
treejamie <me@treejamie.com>
2025-03-27 15:47:29 +0000

    prevent an actual infinite loop

--- 10.1.2-flow-control/infinite.go
+++ 10.1.2-flow-control/infinite.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	// timer one
+	timer := time.NewTimer( 2 * time.Second)
+
 	for {
 		// infinite loop
+
+		// but prevent stupidity
+		<-timer.C
+		fmt.Println("Apologies, but you're being silly. Someone has to be the adult here.")
+		return
 	}
 }
