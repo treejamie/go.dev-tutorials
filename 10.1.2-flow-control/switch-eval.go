4a0ccd337c6d40e546dfcbfc66154be48dc5ce66
treejamie <me@treejamie.com>
2025-03-27 17:40:35 +0000

    yes, case statements do only execute once

---
+++ 10.1.2-flow-control/switch-eval.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When is Saturday?")
+
+	today := time.Now().Weekday()
+	
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("Day after tomorrow.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
