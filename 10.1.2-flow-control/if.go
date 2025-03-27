481822f28248aeba3745bb0127a6ba90e79cfdfd
treejamie <me@treejamie.com>
2025-03-27 14:56:51 +0000

    added a comment about a thing that is easy to miss about the syntax

--- 10.1.2-flow-control/if.go
+++ 10.1.2-flow-control/if.go
@@ -6,7 +6,7 @@ import (
 )
 
 func sqrt(x float64) string {
-	if x < 0 {
+	if x < 0 { // note however the statements don't need to be in parenthesis.
 		return sqrt(-x) + "i"
 	}
 	return fmt.Sprint(math.Sqrt(x))
