37a943725c85d1a2f91c41b067f2be5c6a8073a4
treejamie <me@treejamie.com>
2025-04-01 09:16:08 +0100

    map literals are like struct literals, but you have to provide keys

--- 10.1.3-more-types/maps.go
+++ 10.1.3-more-types/maps.go
@@ -1,3 +1,12 @@
+/*
+Maps
+A map maps keys to values.
+
+The zero value of a map is nil. A nil map has no keys, nor can keys be added.
+
+The make function returns a map of the given type, initialized and ready for use.
+*/
+
 package main
 
 import "fmt"
